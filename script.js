edd87ca277eb1aa95f08f0b5104c58df0d26bf2a
Roman Yaruk <41522683+Jarelino@users.noreply.github.com>
2019-11-02 11:02:17 +0300

    feat: basic buttons click

--- script.js
+++ script.js
@@ -1,24 +1,60 @@
-const buttonsEU = [['`','1','2','3','4','5','6','7','8','9','0','-','=','backspace'],
-                   ['Tab','q','w','e','r','t','y','u','i','o','p','[',']',"&#92;",'Del'],
-                   ['CapsLock','a','s','d','f','g','h','j','k','l',';','&#96;','Enter'],
-                   ['Shift','z','x','c','v','b','n','m','.',',','/','|','Shift'],
-                   ['Ctrl','Win','Alt','SPACE','ALT','<-','|','->','Ctrl']
+const buttonsEU = [['`','1','2','3','4','5','6','7','8','9','0','-','=','Backspace'],
+                   ['Tab','q','w','e','r','t','y','u','i','o','p','[',']',"&#92;",'Delete'],
+                   ['CapsLock','a','s','d','f','g','h','j','k','l',';','&#8242;','Enter'],
+                   ['LShift','z','x','c','v','b','n','m','.',',','/','Up','RShift'],
+                   ['LCtrl','Win','LAlt','Space','RAlt','Left','Down','Right','RCtrl']
 ];
+
+const CapsButtonsEU = [['`','1','2','3','4','5','6','7','8','9','0','-','=','Backspace'],
+                       ['Tab','q','w','e','r','t','y','u','i','o','p','[',']',"&#92;",'Del'],
+                       ['CapsLock','a','s','d','f','g','h','j','k','l',';','&#8242;','Enter'],
+                       ['LShift','z','x','c','v','b','n','m','.',',','/','Up','RShift'],
+                       ['LCtrl','Win','LAlt','Space','RAlt','Left','Down','Right','RCtrl']
+];
+
+const special = ['Meta','Shift','ArrowRight','ArrowLeft','ArrowDown','ArrowUp','Delete', 'Alt', 'Control','Win','Tab', 'CapsLock','RCtrl', 'LCtrl', 'LAlt', 'RAlt', 'Space', 'Up', 'Down', 'Left', 'Backspace', 'Enter', 'Right','RShift','LShift'];
 
 window.onload = () => {
     let body = ""
     body += `<textarea id = "input" rows = "10" columns = "50"></textarea>`
     body += fillElements(buttonsEU);
-
-
     document.getElementsByTagName("body")[0].innerHTML = body;
-}
-buttonsEU.forEach((elem) => {
-    elem.reverse().forEach((elem) => {
-        document.getElementsByTagName("body")[0].insertAdjacentHTML('afterbegin',`<button>${elem}</button>`);
-    })
-})
 
+    Array.from(document.getElementsByClassName("btn")).forEach((elem) => {
+        elem.addEventListener("click", () => {
+            document.getElementById("input").value += isSpecial(elem.innerHTML) ? '' : elem.innerHTML; 
+        })
+
+        elem.addEventListener("mousedown", () => {
+            elem.style.background = "#000";
+            elem.style.color = "#fff";
+        })
+
+        elem.addEventListener("mouseup", () => {
+            elem.style.background = "#fff";
+            elem.style.color = "#000";
+        })
+    })
+
+    document.addEventListener('keydown', function(event) {
+        Array.from(document.getElementsByClassName("btn")).forEach((elem) => {
+            if (elem.innerHTML == event.key) {
+                elem.style.background = "#000";
+                elem.style.color = "#fff";
+            }
+        })
+        document.getElementById("input").value += isSpecial(event.key) ? '' : event.key; 
+    });
+
+    document.addEventListener('keyup', function(event) {
+        Array.from(document.getElementsByClassName("btn")).forEach((elem) => {
+            if (elem.innerHTML == event.key) {
+                elem.style.background = "#fff"
+                elem.style.color = "#000";
+            }
+        })
+    });
+}
 
 function fillElements(arr)
 {
@@ -26,11 +62,17 @@ function fillElements(arr)
 
     arr.forEach((elem) => {
         html += `<div class = "container__row">`
-        elem.reverse().forEach((elem) => {
-            html += `<button>${elem}</button>`
+        elem.forEach((elem) => {
+            html += `<button class = "btn ${isSpecial(elem) ? elem : ''}">${elem}</button>`
         })
         html += `</div>`
     })
 
     return `${html}</div>`
+}
+
+function isSpecial(elem)
+{
+    console.log(special.indexOf(elem));
+    return special.indexOf(elem) + 1;
 }
